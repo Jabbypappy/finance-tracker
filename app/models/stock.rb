8af87b7f5c058c376c61bb848de806e6a91b5bf4
Tristan Lee <leetristan2000@gmail.com>
2021-07-22 02:44:47 -0700

    UserStock many-to-many associations added

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true # When a Stock is being saved, it must had a valid name and a valid ticker_symbol
 
   def self.new_lookup(ticker_symbol) # Self means can directly call this method from the class, don't need an object.
     client = IEX::Api::Client.new(
