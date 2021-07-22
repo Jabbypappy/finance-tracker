c86c92f9c4cc9654be8d0cd20559bfab54258f18
Tristan Lee <leetristan2000@gmail.com>
2021-07-21 19:26:21 -0700

    Flash errors adjusted for JavaScript, stocks now shown via JavaScript & Ajax so as to not reload the entire page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,15 +3,21 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present? # If it's not empty
       @stock = Stock.new_lookup(params[:stock])
-      if @stock
-        render 'users/my_portfolio'
+      if @stock # If it isn't nil, see stock.rb begin and rescue. If rescued, we set it to nil.
+        respond_to do |format|
+          format.js { render partial: 'users/result' } # This will expect a partial that is a js file
+        end
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search" # A whole cycle is not being completed when using Ajax, since the whole page is not being reloaded, so we must use flash.now in order for the alert to only be displayed once, otherwise it will persist for another search of a different ticker as well, which we don't want.
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 
