30b8b93a5f31a5ce8ef90016b6a3fe97bfede528
Tristan Lee <leetristan2000@gmail.com>
2021-07-26 22:31:43 -0700

    Added friend functionality to follow other users and view stocks they're tracking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,38 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
   def friends
     @friends = current_user.friends
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
+  def search
+    if params[:friend].present? # If it's not empty
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' } # This will expect a partial that is a js file
+        end
+      else
+        respond_to do |format|
+          flash.now[:jsalert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:jsalert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,31 @@ class User < ApplicationRecord
     "Anonymous" # If the return is passed because there's no first_name or last_name, then "Anonymous" is reached, and it is therefore returned as the last part of the function
   end
 
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq # .uniq means if we get the same person from multiple of these returning something, then it will remove duplicates
+    return nil unless to_send_back
+    to_send_back
+  end
+  def self.first_name_matches(param)
+    matches("first_name", param)
+  end
+  def self.last_name_matches(param)
+    matches("last_name", param)
+  end
+  def self.email_matches(param)
+    matches("email", param)
+  end
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id}
+  end
+
+  def not_friends_with?(id_of_friend)
+    !self.friends.where(id: id_of_friend).exists?
+  end
+
 end
